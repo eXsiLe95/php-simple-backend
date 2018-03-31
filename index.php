e617db334a2f16a5e035776d137afe4b1edc9023
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-03-31 15:28:38 +0200

    Catch GET, POST, PUT and DELETE requests

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+// ROUTES #########################################################################################
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+	// READ: Get request to fetch data from server
+	echo "GET";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	// CREATE: Post request to server to create new data
+	echo "POST";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+	// UPDATE: Put request to update data on server
+	echo "PUT";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	// DELETE: Delete request to delete data from server
+	echo "DELETE";
+}
